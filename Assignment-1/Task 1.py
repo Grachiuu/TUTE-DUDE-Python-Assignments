8b37a5348b7e564bf94a822204b7ecad04fd2e61
Grachiuu <gargdivy1@gmail.com>
2025-07-14 17:57:07 +0530

    Add files via upload

---
+++ Assignment-1/Task 1.py
@@ -0,0 +1,13 @@
+# Task:1 'Perform Basic Mathematical Operations
+a=input("Enter the first number: ")
+b=input("Enter the second number: ")
+a=float(a)
+b=float(b)
+x=a+b
+y=a-b
+z=a*b
+c=a//b
+print('Addition: ' ,x)
+print('Subtraction: ',y)
+print('Multiplication: ',z)
+print('Division: ',c)
---
+++ Assignment-1/Task 2.py
@@ -0,0 +1,7 @@
+#Create A Personalized Greeting
+
+x=input("Enter your first name: ")
+y=input("Enter your last name: ")
+
+print("Hello,"+" "+x+" "+y+'!'+" Welcome to the Python Program.")
+
